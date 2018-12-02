a78d62855f7fbf07491cafec40bb690dc944aa4a
ri.calgaro <ri.calgaro@gmail.com>
2018-12-02 20:36:34 -0300

    search filter multiple checkbox

--- public/scripts/public-index.js
+++ public/scripts/public-index.js
@@ -3,12 +3,12 @@ var tipo_control = 'navegacion';
 
 function changeVisibility(checkbox, id) {
     layers[id].setVisible(checkbox.checked);
-    if(checkbox.checked){
-        document.querySelector("#leyend_"+checkbox.id).classList.remove("d-none");
+    if (checkbox.checked) {
+        document.querySelector("#leyend_" + checkbox.id).classList.remove("d-none");
     } else {
-        document.querySelector("#leyend_"+checkbox.id).classList.add("d-none");
+        document.querySelector("#leyend_" + checkbox.id).classList.add("d-none");
     }
-    
+
     if (checkbox.checked && tipo_control == 'consulta') {
         capa_activa = checkbox.id;
         setAllInvisible(id);
@@ -20,7 +20,7 @@ function setAllInvisible(id) {
         const element = layers[index];
         if (index != id) {
             element.setVisible(false);
-            document.querySelector("#leyend_"+data[index-1].capa).classList.add("d-none")
+            document.querySelector("#leyend_" + data[index - 1].capa).classList.add("d-none")
         }
     }
 }
@@ -82,13 +82,13 @@ var seleccionarControl = function (el) {
     tipo_control = el.value;
 
     if (el.value == "consulta") {
-       //Seteo todas las capas como no visible, para que la consulta se haga sobre una sola capa;
+        //Seteo todas las capas como no visible, para que la consulta se haga sobre una sola capa;
         capa_activa = ''
         setAllInvisible(0);
 
         map.removeInteraction(draw);
         map.addInteraction(selectInteraction);
-        
+
         map.un('click');
         map.un('pointermove', pointerMoveHandler);
         map.on('click', clickEnMapa);
@@ -96,7 +96,7 @@ var seleccionarControl = function (el) {
     } else if (el.value == "navegacion") {
         map.removeInteraction(selectInteraction);
         map.removeInteraction(draw);
-        
+
         map.un('click');
         map.un('pointermove', pointerMoveHandler);
         map.un('click', clickEnMapa);
@@ -113,15 +113,39 @@ var seleccionarControl = function (el) {
         });
 
         addInteraction();
-    } else if (el.value == "agregar"){
+    } else if (el.value == "agregar") {
         map.removeInteraction(selectInteraction);
         map.removeInteraction(draw);
 
         map.un('pointermove', pointerMoveHandler);
         map.un('click', clickEnMapa);
 
-        map.on('click',function(event) {
+        map.on('click', function (event) {
             addPoint(event)
         });
     }
 };
+
+
+//  Checkboxes search filter
+(function ($) {
+    $(".keyword").on('keyup', function (e) {
+        var $this = $(this);
+        var exp = new RegExp($this.val(), 'i');
+        $(".capas-list li label").each(function () {
+            var $self = $(this);
+            if (!exp.test($self.text())) {
+                $self.parent().hide();
+            } else {
+                $self.parent().show();
+            }
+        });
+    });
+})(jQuery);
+
+//  Ver/Ocultar controles div
+function toggleDivControles(){
+    document.querySelector("#caret-up").classList.toggle("d-none")
+    document.querySelector("#caret-down").classList.toggle("d-none")
+    document.querySelector("#controles").classList.toggle("d-none")
+}
--- public/scripts/public-mouse-position.js
+++ public/scripts/public-mouse-position.js
@@ -3,7 +3,7 @@ var scaleLineControl = new ol.control.ScaleLine();
 map.addControl(scaleLineControl);
 
 var mousePositionControl = new ol.control.MousePosition({
-    coordinateFormat: ol.coordinate.createStringXY(47),
+    coordinateFormat: ol.coordinate.createStringXY(31),
     projection: 'EPSG:3857',
     className: 'custom-mouse-position',
     target: document.getElementById('mouse-position'),
